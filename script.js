8e0afe7e870ab8d9f6e1f5bdb6218f3485f90325
Gustavo <gustavoorlando.1d2015@gmail.com>
2022-11-30 15:06:35 -0300

    commit resolvendo saldo

--- script.js
+++ script.js
@@ -27,25 +27,48 @@ function fechar3() {
 }
 //Saldo
 alert("*Primeiro Acesso* Você ganhou 30 reais em saldo para comprar tickets");
+var saldo = document.getElementById('idSaldo');
+saldo = 30;
+var michael = document.querySelector('.btn-modal-finalizar');
+michael = 20;
+var queen = document.querySelector('.btn-modal-finalizar2');
+queen = 10;
+var anitta = document.querySelector('.btn-modal-finalizar3');
+anitta = 20;
 const btn = document.querySelector('.btn-modal-finalizar');
 const btn2 = document.querySelector('.btn-modal-finalizar2');
 const btn3 = document.querySelector('.btn-modal-finalizar3');
 btn.addEventListener('click',function(){
     alert("Compra Realizada com Sucesso");
-    document.getElementById("idSaldo").textContent = "10";
+    saldo = saldo - 20;
+    document.getElementById("idSaldo").textContent = saldo;
+    if (saldo <= 0) {
+        document.getElementById("idSaldo").textContent = "Sem Saldo";
+        alert("Saldo Zero");
+    }
 });
 btn2.addEventListener('click',function(){
     alert("Compra Realizada com Sucesso");
-    document.getElementById("idSaldo").textContent = "20";
+    saldo = saldo - 10;
+    document.getElementById("idSaldo").textContent = saldo;
+    if (saldo <= 0) {
+        document.getElementById("idSaldo").textContent = "Sem Saldo";
+        alert("Saldo Zero");
+    }
 });
 btn3.addEventListener('click',function(){
     alert("Compra Realizada com Sucesso");
-    document.getElementById("idSaldo").textContent = "10";
+    saldo = saldo - 20;
+    document.getElementById("idSaldo").textContent = saldo;
+    if (saldo <= 0) {
+        document.getElementById("idSaldo").textContent = "Sem Saldo";
+        alert("Saldo Zero");
+    }
 });
 //sair
 const sair = document.querySelector('.botao-sair');
 function redirecionar(params) {
-    location.href = "login.html";
+    location.href = "index.html";
 }
 sair.addEventListener('click',function(){
     alert("Deslogado com Sucesso");
